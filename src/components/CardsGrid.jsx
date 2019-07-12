57a8b8910507dfdefe72799ca82f0df656b0ace8
Oscar Rodríguez <oscardavidrodriguezm@gmail.com>
2019-07-12 12:19:18 -0500

    CardGrid template to load cards inside each view dynamically.

--- src/components/CardsGrid.jsx
+++ src/components/CardsGrid.jsx
@@ -6,17 +6,39 @@ class CardsGrid extends Component {
   state = {
     cards: [],
     seenIds: new Map(), //add some memo to improve app performance
+    seenPaths: new Map(),
     count: 0,
-    prev: "",
-    next: ""
+    prev: null,
+    next: `${process.env.API_URL}${this.props.path}/?page=2`,
+    path: `${process.env.API_URL}${this.props.path}/?page=1`,
+    page: 1
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = async (path = this.state.path) => {
+    const { seenPaths, cards } = this.state;
+    if (path && seenPaths.has(path)) {
+      const { p, n } = seenPaths.get(path);
+      return this.setState({ prev: p, next: n });
+    }
+
+    const { data: response } = await http.get(path);
+    const { results: newCards, count, previous: prev, next } = response;
+    seenPaths.set(path, { p: prev, n: next });
+    this.setState({ count, cards: [...cards, ...newCards], prev, next });
   };
 
   renderGrid = cards => {
+    const { page } = this.state;
+    const pageCards = cards.slice((page - 1) * 10, page * 10);
     return (
-      <div className='container-fluid my-3 mx-auto'>
+      <div className='container-fluid my-3'>
         <div className='card-columns'>
-          {cards &&
-            cards.map((card, i) => (
+          {pageCards &&
+            pageCards.map((card, i) => (
               <Card
                 key={i}
                 id={i}
@@ -30,11 +52,9 @@ class CardsGrid extends Component {
   };
 
   handleCardSelect = async id => {
-    console.log(this.state);
     const { cards, seenIds } = this.state;
     if (seenIds.has(id)) {
       //If info has been already requested, there's no need to query again
-      console.log(cards);
       return this.setState({ cards }); //card info is already available in cards
     } else seenIds.set(id, id);
 
@@ -72,6 +92,17 @@ class CardsGrid extends Component {
 
     console.log(cards);
     this.setState({ cards });
+  };
+
+  handlePageChange = async (dir, page) => {
+    const { prev, next } = this.state;
+    if (dir === "next" && next) {
+      this.getData(next);
+      this.setState({ page: page + 1 });
+    } else if (dir === "prev" && prev) {
+      this.getData(prev);
+      this.setState({ page: page > 1 ? page - 1 : page });
+    }
   };
 }
 
