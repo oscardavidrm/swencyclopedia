b0bf0cf79a4949a15375f516c3c9de019271aafe
Oscar Rodríguez <oscardavidrodriguezm@gmail.com>
2019-07-15 09:42:11 -0500

    Changed image requiring technique for require().

--- src/components/utils/Loader.jsx
+++ src/components/utils/Loader.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import gif from "./images/loader.gif";
 
 const Loader = () => {
   return (
     <div className='container-fluid'>
       <div className='d-flex justify-content-center'>
-        <img src={gif} alt='loading...' />
+        <img src={require("./images/loader.gif")} alt='loading...' />
       </div>
     </div>
   );
