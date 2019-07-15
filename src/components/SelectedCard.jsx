61db3f1cbef85fde14fe530b1a7b1d2c71d7befc
Oscar Rodríguez <oscardavidrodriguezm@gmail.com>
2019-07-14 22:53:03 -0500

    Turned SelectedCard sfc into a component to handle character info fetching and displaying.

--- src/components/SelectedCard.jsx
+++ src/components/SelectedCard.jsx
@@ -1,43 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
+import http from "../services/http";
+import Loader from "./utils/Loader.jsx";
 
-const SelectedCard = ({ card: content }) => {
-  return (
-    <React.Fragment>
-      {content && (
-        <div className='card'>
-          <div className='card-body'>
-            <h4 className='card-title'>{content.name}</h4>
-            <p className='card-text font-weight-light'>
-              origin: {content.homeworld}
-            </p>
-            <p className='card-text font-weight-light'>
-              {content.species && `species: ${content.species}`}
-            </p>
-            <p className='card-text font-weight-light'>
-              birth: {content.birth_year}
-            </p>
-            <p className='card-text font-weight-light'>
-              gender: {content.gender}
-            </p>
-            <p className='card-text font-weight-light'>
-              height: {content.height}
-            </p>
-            <p className='card-text font-weight-light'>mass: {content.mass}</p>
-            <p className='card-text font-weight-light'>
-              {content.vehicles.length > 0 && `vehicles: ${content.vehicles}`}
-            </p>
-            <p className='card-text font-weight-light'>
-              {content.starships.length > 0 &&
-                `starships: ${content.starships}`}
-            </p>
-            <p className='card-text font-weight-light'>
-              {content.films.length > 0 && `films: ${content.films}`}
-            </p>
+class SelectedCard extends Component {
+  state = {
+    loading: null
+  };
+
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      this.setState({ loading: true });
+      this.handleSelectedCard();
+    }
+  }
+
+  render() {
+    const { cards, selectedCardId: id } = this.props;
+    const { loading } = this.state;
+    const card = cards[id];
+    return (
+      <React.Fragment>
+        {loading && <Loader />}
+        {!loading && card && (
+          <div id='selectedCard' className='card'>
+            <div className='card-body'>
+              <h4 className='card-title'>{card.name || card.title}</h4>
+              {Object.keys(card).map(key => (
+                <p key={key} className='card-text font-weight-light'>
+                  {key}: {card[key]}
+                </p>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-    </React.Fragment>
-  );
-};
+        )}
+      </React.Fragment>
+    );
+  }
+
+  handleSelectedCard = async () => {
+    const { cards, seenIds, selectedCardId: id } = this.props;
+    if (seenIds.has(id)) {
+      //If info has been already requested, there's no need to query again
+      return this.setState({ loading: false }); //card info is already available in cards
+    } else seenIds.set(id, id);
+
+    const card = { ...cards[id] };
+    for (const key in card) {
+      if (key === "created" || key === "edited" || key === "url") {
+        delete card[key];
+        continue;
+      }
+
+      if (typeof card[key] === "object") {
+        //this means it contains an endpoint
+        let data = [];
+        for (const d of card[key]) {
+          const { data: res } = await http.get(d);
+          data.push(res.title || res.name);
+        }
+
+        card[key] = data;
+        continue;
+      }
+
+      if (card[key].indexOf("https") !== -1) {
+        const { data: res } = await http.get(card[key]);
+        card[key] = res.title || res.name;
+        continue;
+      }
+    }
+
+    cards[id] = { ...card };
+    this.setState({ loading: false });
+  };
+}
 
 export default SelectedCard;
