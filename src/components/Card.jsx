1c1443f04a386da410d0eee1a4cf369219ea5781
Oscar Rodríguez <oscardavidrodriguezm@gmail.com>
2019-07-14 21:34:11 -0500

    Card and CardsGrid components to manage cards.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-const Card = ({ id, content, onCardSelect }) => {
+const Card = ({ id, selectedClass, content, onCardSelect }) => {
   return (
-    <div onClick={() => onCardSelect(id)} className='card'>
-      <div style={{ cursor: "pointer" }} className='card-body'>
+    <div
+      onClick={() => onCardSelect(id)}
+      className={`card ${selectedClass.border}`}
+    >
+      <div
+        style={{ cursor: "pointer" }}
+        className={`card-body ${selectedClass.text}`}
+      >
         <h5 className='card-title'>{content.name}</h5>
       </div>
     </div>
--- src/components/CardsGrid.jsx
+++ src/components/CardsGrid.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import http from "../services/http";
-import ResourcesBar from "./ResourcesBar.jsx";
 import PaginationSelector from "./PaginationSelector.jsx";
 import Card from "./Card.jsx";
-import NavBar from "./Navbar.jsx";
+import Loader from "./utils/Loader.jsx";
+import WikipediaBox from "./WikipediaBox.jsx";
 
 class CardsGrid extends Component {
   state = {
@@ -15,7 +15,8 @@ class CardsGrid extends Component {
     prev: null,
     next: `${process.env.API_URL}${this.props.path}/?page=2`,
     path: `${process.env.API_URL}${this.props.path}/?page=1`,
-    page: 1
+    page: 1,
+    loading: this.props.loading
   };
 
   componentDidMount() {
@@ -23,100 +24,69 @@ class CardsGrid extends Component {
   }
 
   getData = async (path = this.state.path) => {
+    this.setState({ loading: true });
     const { seenPaths, cards } = this.state;
+    const { onMount } = this.props;
+
     if (path && seenPaths.has(path)) {
       const { p, n } = seenPaths.get(path);
-      return this.setState({ prev: p, next: n });
+      return this.setState({ prev: p, next: n, loading: false });
     }
 
     const { data: response } = await http.get(path);
     const { results: newCards, count, previous: prev, next } = response;
     seenPaths.set(path, { p: prev, n: next });
-    this.setState({ count, cards: [...cards, ...newCards], prev, next });
-  };
 
-  renderNavBar = headers => {
-    return <NavBar headers={headers} />;
-  };
-
-  renderResourcesBar = headers => {
-    return (
-      <div className='col-3 my-3'>
-        {/* render navigation bar */}
-        <ResourcesBar headers={headers} />
-      </div>
-    );
+    onMount(count); //pass data to parent
+    this.setState({
+      count,
+      cards: [...cards, ...newCards],
+      prev,
+      next,
+      loading: false
+    });
   };
 
   renderGrid = cards => {
-    const { prev, next, page } = this.state;
+    const { selectedCardId, prev, next, page, loading } = this.state;
     const pageCards = cards.slice((page - 1) * 10, page * 10);
     return (
-      <div className='col-6 my-3'>
+      <div className='col-7 my-3'>
         {/* render main grid bar */}
-        <div className='container-fluid'>
-          <div className='card-columns'>
-            {pageCards &&
-              pageCards.map((card, i) => (
-                <Card
-                  key={i + (page - 1) * 10}
-                  id={i + (page - 1) * 10}
-                  content={card}
-                  onCardSelect={this.handleCardSelect}
-                />
-              ))}
+        {loading && <Loader />}
+        {!loading && (
+          <div className='container-fluid'>
+            <div className='card-columns'>
+              {pageCards &&
+                pageCards.map((card, i) => (
+                  <Card
+                    key={i + (page - 1) * 10}
+                    id={i + (page - 1) * 10}
+                    selectedClass={
+                      i + (page - 1) * 10 === selectedCardId
+                        ? { border: "border-danger", text: "text-danger" }
+                        : ""
+                    }
+                    content={card}
+                    onCardSelect={this.onCardSelect}
+                  />
+                ))}
+            </div>
           </div>
-        </div>
+        )}
         <PaginationSelector
           prev={prev}
           next={next}
           page={page}
           onPageChange={this.handlePageChange}
         />
+        <WikipediaBox query={cards[selectedCardId]} />
       </div>
     );
   };
 
-  handleCardSelect = async id => {
-    const { cards, seenIds } = this.state;
-    if (seenIds.has(id)) {
-      //If info has been already requested, there's no need to query again
-      return this.setState({ selectedCardId: id, cards }); //card info is already available in cards
-    } else seenIds.set(id, id);
-
-    const card = { ...cards[id] };
-
-    const { data: homeworld } = await http.get(card.homeworld);
-
-    let films = [];
-    for (const f of card.films) {
-      const { data: film } = await http.get(f);
-      films.push(film.title);
-    }
-    let species = [];
-    for (const s of card.species) {
-      const { data: specie } = await http.get(s);
-      species.push(specie.name);
-    }
-    let starships = [];
-    for (const s of card.starships) {
-      const { data: starship } = await http.get(s);
-      starships.push(starship.name);
-    }
-    let vehicles = [];
-    for (const v of card.vehicles) {
-      const { data: vehicle } = await http.get(v);
-      vehicles.push(vehicle.name);
-    }
-
-    card.films = films;
-    card.species = species;
-    card.starships = starships;
-    card.vehicles = vehicles;
-    card.homeworld = homeworld.name;
-    cards[id] = { ...card };
-
-    this.setState({ cards, selectedCardId: id });
+  onCardSelect = id => {
+    this.setState({ selectedCardId: id });
   };
 
   handlePageChange = async (dir, page) => {
